be02849121031c500b89828f84e6dc22d67bef1e
rovsencode <rovshanakh@code.edu.az>
2022-08-18 18:49:36 +0400

    task1

---
+++ ConsoleApp1/ExtentionMethods.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+   static class ExtentionMethods
+    { 
+        public static bool isOdd(this int num)
+        {
+
+
+            if (num % 2 != 0 && num>0)
+            {
+                return true;
+            }
+            else 
+               return false;
+        }
+        public static bool isEven(this int num)
+        {
+
+
+            if (num % 2 == 0 && num > 0)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        public static bool IsContainsDigit(this string str)
+        {
+            bool check = false; ;
+            foreach (var item in str)
+            {
+                if (char.IsDigit(item) == true)
+                {
+                    check = true;
+                    return check;
+                }
+            }
+            if (check)
+                return true;
+            else
+                return false;
+            }
+
+
+        
+        
+        public static string ToCapitalize(this string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(char.ToUpper(str[0]));
+            sb.Append((str.Substring(1).ToLower()));
+            return sb.ToString();
+
+        }
+        public static void GetValueIndexes(this string str,char ch)
+        {
+            int[] arr = new int[0];
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i]==ch)
+                {
+                    Array.Resize<int>(ref arr, arr.Length+1);
+                    arr[arr.Length - 1] = i;
+                }
+
+              
+            }
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        
+
+        }
+    }
+}
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(5.isOdd());
+            Console.WriteLine(6.isEven());
+            Console.WriteLine("code2017".IsContainsDigit());
+            Console.WriteLine("coders".ToCapitalize());
+            "codeacademy".GetValueIndexes('c');
+           
+        }
+    }
+}
